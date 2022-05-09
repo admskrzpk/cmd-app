fab4e907fe7f5138f68fb7513f0d205a52e2366a
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-05-09 13:20:47 +0200

    working app

--- src/main/scala/ArgsFromFolder.scala
+++ src/main/scala/ArgsFromFolder.scala
@@ -1,13 +1,13 @@
 import java.io.File
 
 object ArgsFromFolder extends App {
-  def getListOfFiles(dir: String):List[File] = {
+  def getListOfFiles(dir: String) = {
     val d = new File(dir)
     if (d.exists && d.isDirectory) {
-      d.listFiles.toList
+      d.listFiles.map(_.getName).toList.foreach(println)
     } else {
       List[File]()
     }
   }
-println(getListOfFiles(args(0)))
+getListOfFiles(args(0))
 }
